6ef0ff865531e127e5c330fc5fb233d0bbfdbdb3
Tristan Wojtas <tristanwojtasdev@gmail.com>
2024-07-29 00:13:24 -0400

    Added Pandas, MaldiPy, Seaborn and the import data and import data structure

--- Pathogen/Pathogen.py
+++ Pathogen/Pathogen.py
@@ -1,17 +1,32 @@
+import sys
+import os
+import pandas as pd
+sys.path.append(os.path.dirname(os.path.abspath(__file__)))
+
+# Import the install method from the Setup module in the Dependencies package
+from Dependencies.Setup import install
+
 def main():
     print("Hello Pathogen Project!")
+    install('seaborn')
+    import seaborn
+    install('MALDIpy==0.1.1')
+    import MALDIpy
+    print("successful!")
+    raw_file = pd.read_csv('Data/Import/info-ecoli-MAI.csv')
+    print(raw_file.head())
 
 if __name__ == "__main__":
     main()
 
 # 1. Move Import
 # Open the file in read mode
-with open('Pathogen/TestData/air.POSITIVE.txt', 'r') as file:
-    # Read the contents of the file
-    contents = file.read()
+# with open('Pathogen/TestData/air.POSITIVE.txt', 'r') as file:
+#     # Read the contents of the file
+#     contents = file.read()
 
 # Print the contents
-print(contents)
+# print(contents)
 
 #2. Quality Control
 
