682548565b1f7f6de68a87e1be35992fa1dec607
Tristan Wojtas <tristanwojtasdev@gmail.com>
2024-08-27 20:17:49 -0400

    Developed through baseline

--- Pathogen/BaselineCorrection/BaselineCorrection.py
+++ Pathogen/BaselineCorrection/BaselineCorrection.py
@@ -1,6 +1,13 @@
-class my_class(object):
-    pass
+import numpy as np
+import pandas as pd
+from scipy.signal import savgol_filter
 
-
-
-
+def snip_baseline_correction(signal, iterations=150):
+    n = len(signal)
+    baseline = np.copy(signal)
+    
+    for i in range(iterations):
+        for j in range(1, n - 1):
+            baseline[j] = min(baseline[j], (baseline[j - 1] + baseline[j + 1]) / 2)
+    
+    return baseline
